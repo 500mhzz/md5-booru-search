2d8c4f8515171727974cc93e1542da6f56737323
500mhzz <its500mhz@proton.me>
2024-05-20 01:40:18 +0300

    UI Dark redesign

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,27 +4,29 @@
 
 
 <div>
-	<nav class="w-full bg-blue-700 p-4 top-0 flex justify-between items-center">
-		<a href="/"><h1 class="text-2xl text-center text-white">MD5 Booru Search</h1></a>
-		<div class="text-sm">
-			<a href="https://github.com/500mhzz/md5-booru-search" class="text-white hover:underline mr-4"
+	<nav class="w-full bg-neutral-900/10 backdrop-blur-lg fixed p-4 top-0 flex justify-between items-center border-b border-white/10">
+		<a href="/">
+            <!-- <h1 class="text-2xl text-center text-white">MD5 Booru Search</h1> -->
+            <img src="favicon.png" alt="icon" class="w-[3.3rem] border border-white/10 rounded-full">
+        </a>
+        <div>
+            <a href="https://github.com/500mhzz/md5-booru-search" class="text-white/90 hover:underline mr-4 text-xl"
 				>GitHub</a
+			>
+			<a href="https://discord.gg/hn2Hdd4cb8" class="text-white/90 hover:underline mr-4 text-xl"
+				>Discord</a
 			>
 		</div>
 	</nav>
 
-    <div class="bg-blue-800 p-2 text-center mt-2 mx-2 rounded-lg">
-        If you have any suggestions or issues, please open a ticket in our <a href="https://discord.gg/hn2Hdd4cb8" class="text-sky-300 hover:underline">Discord</a>.
-    </div>
-
 	<main 
-        class="flex flex-col items-center justify-center px-4"
-        style="min-height: calc(100vh - 17vh);"
+        class="flex flex-col items-center justify-center px-4 mt-10"
+        style="min-height: calc(100vh - 9.7vh);"
     >
 		<slot />
 	</main>
 </div>
 
-<footer class="bg-blue-700 p-4 text-center text-white">
+<footer class="bg-neutral-900/10 p-4 text-center text-white border-t border-white/10">
         <p>MD5 Search created with 💖 by <a href="https://500mhz.xyz" class="text-sky-300 hover:underline">500mhz</a></p>
-</footer>
+</footer> 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,72 +1,104 @@
 <script lang="ts">
-    import type { ActionData } from './$types';
+	import type { ActionData } from './$types';
 
-    export let form: any;
+	export let form: any;
 </script>
 
 <svelte:head>
-    <title>MD5 Booru Search</title> 
+	<title>MD5 Booru Search</title>
 </svelte:head>
 
 <div class="mt-16 w-full px-4 md:px-0 md:w-3/4 lg:w-1/2 xl:w-1/3 2xl:w-1/4">
-    {#if form?.imageUrl}
-        <img
-            src={form.imageUrl}
-            alt="Preview"
-            class="w-full h-auto object-cover rounded-md shadow-lg mb-10"
-        />
-        <p class="text-center">MD5: {form.parentMD5}</p>
-    {/if}
+	{#if form?.imageUrl}
+		<img
+			src={form.imageUrl}
+			alt="Preview"
+			class="w-full h-auto object-cover rounded-lg border border-white/10 shadow-lg mb-3"
+		/>
+		<p class="text-center text-sm bg-neutral-900/35 rounded-lg px-5 py-3 border border-white/10 sm:text-md">
+            <span class="text-white/60">MD5:</span> {form.parentMD5}
+        </p>
+	{/if}
 </div>
 
-<div class="w-full px-4 md:px-0 md:w-3/4 lg:w-1/2 xl:w-1/3 2xl:w-1/4">
-    <form action="?/search" method="POST" class="flex items-center justify-center mt-10">
-        <input
-            type="text"
-            name="q"
-            placeholder="Search"
-            class="flex-grow mr-4 py-2 px-4 rounded-md border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-gray-800 text-white"
-        />
-        <button type="submit" class="py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-700"
-            >Search</button
-        >
-    </form>
-
-    <div class="mt-10 bg-gray-800 p-4 rounded-md shadow-lg flex flex-col items-center max-w-full mb-10">
-        {#if form?.r34}
-            <h2 class="text-2xl text-center">Rule34</h2>
-            <div class="mt-5 space-y-2">
-                {#each form.r34 as post}
-                    <a
-                        href={`https://rule34.xxx/index.php?page=post&s=view&id=` + post.id}
-                        class="text-blue-500 hover:underline block">Link</a
-                    >
-                {/each}
-            </div>
-        {/if}
-
-        {#if form?.tbib}
-            <h2 class="text-2xl text-center mt-10">TBIB (The Big ImageBoard)</h2>
-            <div class="mt-5 space-y-2">
-                {#each form.tbib as post}
-                    <a
-                        href={`https://tbib.org/index.php?page=post&s=view&id=` + post.id}
-                        class="text-blue-500 hover:underline block">Link</a
-                    >
-                {/each}
-            </div>
-        {/if}
-
-        {#if form?.e621}
-            <h2 class="text-2xl text-center mt-10">E621</h2>
-            <div class="mt-5 space-y-2">
-                {#each form.e621.posts as post}
-                    <a
-                        href={`https://e621.net/posts/` + post.id}
-                        class="text-blue-500 hover:underline block">Link</a
-                    >
-                {/each}
-            </div>
-        {/if}
+{#if !form}
+    <div class="flex flex-col items-center justify-center mt-10">
+        <h1 class="flex flex-row items-center gap-4 text-3xl text-white">
+            MD5 Booru Search
+        </h1>
+        <p class="text-white/60 text-center mt-3">Search for images on Rule34, TBIB, and E621 using the MD5 hash of the image.</p>
     </div>
+{/if}
+
+<div class="flex flex-col w-full px-4 md:px-0 md:w-3/4 lg:w-1/2 xl:w-1/3 2xl:w-1/4 gap-3">
+	<form action="?/search" method="POST" class="flex items-center justify-center mt-10 sm:text-md text-sm">
+		<input
+			type="text"
+			name="q"
+			placeholder="Input Booru URL Here"
+			class="flex-grow mr-2 py-2 px-4 rounded-lg border border-white/10 focus:outline-none focus:ring-2 focus:ring-white/35 bg-neutral-900/35 text-white"
+		/>
+		<button type="submit" class="py-2 px-4 bg-white/90 text-black rounded-lg hover:bg-white/100"
+			>Search</button
+		>
+	</form>
+
+	<div class="flex flex-col items-center max-w-full gap-5">
+		{#if form?.r34}
+            <div class="flex flex-row bg-neutral-900/35 border border-white/10 p-4 rounded-lg shadow-lg w-full">
+				<img src="rule34-logo.png" alt="Rule34 Logo" class="w-20 mr-0" />
+
+				<div class="ml-5 w-full">
+					<h2 class="text-xl">Rule34</h2>
+						{#each form.r34 as post}
+							<a
+                            href={`https://rule34.xxx/index.php?page=post&s=view&id=` + post.id}
+								class="text-black bg-white/90 rounded-lg p-2 mt-2 hover:bg-white/100 block ml-0 text-center w-full">Go to Rule34</a
+							>
+						{/each}
+				</div>
+			</div>
+		{/if}
+
+		{#if form?.tbib}
+			<div class="flex flex-row bg-neutral-900/35 border border-white/10 p-4 rounded-lg shadow-lg w-full">
+				<img src="tbib-logo.png" alt="TBIB Logo" class="w-20 mr-0" />
+
+				<div class="ml-5 w-full">
+					<h2 class="text-xl">TBIB (The Big ImageBoard)</h2>
+						{#each form.tbib as post}
+							<a
+								href={`https://tbib.org/index.php?page=post&s=view&id=` + post.id}
+								class="text-black bg-white/90 rounded-lg p-2 mt-2 hover:bg-white/100 block ml-0 text-center w-full">Go to TBIB</a
+							>
+						{/each}
+				</div>
+			</div>
+		{/if}
+
+		{#if form?.e621}
+
+            <div class="flex flex-row bg-neutral-900/35 border border-white/10 p-4 rounded-lg shadow-lg w-full">
+				<img src="e621-logo.png" alt="Rule34 Logo" class="w-20 mr-0" />
+
+				<div class="ml-5 w-full">
+					<h2 class="text-xl">E621</h2>
+						{#each form.e621.posts as post}
+							<a
+                            href={`https://e621.net/posts/` + post.id}
+								class="text-black bg-white/90 rounded-lg p-2 mt-2 hover:bg-white/100 block ml-0 text-center w-full">Go to E621</a
+							>
+						{/each}
+				</div>
+			</div>
+		{/if}
+	</div>
+
+	<div class="bg-white/10 text-white/85 text-center backdrop-blur-lg mb-10 p-3 rounded-lg w-fit">
+		For suggestions or bug reports please join our <a
+			href="https://discord.gg/hn2Hdd4cb8"
+			class="text-sky-300 hover:underline">Discord</a
+		>
+		and open a ticket.
+	</div>
 </div>
