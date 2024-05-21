1c08b90ad3e9ffcdf59442faa6c0f3e9bbd9be83
500mhzz <its500mhz@proton.me>
2024-05-21 19:20:01 +0300

    Remove vercel speed insights cause dumb

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,8 @@
 
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
-	import { injectSpeedInsights } from '@vercel/speed-insights/';
 
 	inject({ mode: dev ? 'development' : 'production' });
-	injectSpeedInsights();
 </script>
 
 <div>
